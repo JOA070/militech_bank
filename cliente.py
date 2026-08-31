ba140f9c0c27cf87dcb383f26f7d46933de51d69
Joao Victor <victor.nascimento@dcomp.ufs.br>
2026-08-31 16:08:08 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,1 +1,13 @@
+def cadastrar_cliente(nome,cpf):
+  return nome
+  return cpf
 
+def consultar_cliente(cpf):
+  if cpf == cpf_cadastrado:
+    print ('O cliente tem conta')
+  else:
+    print ('Cliente não encontrado')
+
+def criar_conta(cpf,numero_conta,tipo_conta,saldo_inicial):
+  numero_conta = 1
+  return cpf,numero_conta,tipo_conta,saldo_inicial
