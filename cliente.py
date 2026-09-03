f9a4f1345fc2b0dc4cf83505769ab7f7a07a9e27
Joao Victor <victor.nascimento@dcomp.ufs.br>
2026-09-03 10:09:54 -0300

    Implementação das funções do cliente

--- cliente.py
+++ cliente.py
@@ -6,7 +6,7 @@ def consultar_cliente(cpf):
   if cpf == cpf_cadastrado:
     print ('O cliente tem conta') 
   else:
-    print ('Cliente não encontrado')
+    print ('Cliente não encontrado') 
 
 def criar_conta(cpf,numero_conta,tipo_conta,saldo_inicial):
   numero_conta = 1 
