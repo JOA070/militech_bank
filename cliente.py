a7fde729749cabc89cf69dd267ddcf99187c620c
Joao Victor <victor.nascimento@dcomp.ufs.br>
2026-09-03 20:14:55 -0300

    retirada do valor de numero_conta

--- cliente.py
+++ cliente.py
@@ -8,5 +8,4 @@ def consultar_cliente(cpf):
     print ('Cliente não encontrado') 
 
 def criar_conta(cpf,numero_conta,tipo_conta,saldo_inicial):
-  numero_conta = 1 
   return cpf,numero_conta,tipo_conta,saldo_inicial
