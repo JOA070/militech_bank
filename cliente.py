8e1e7a7e0f6456a8efaeaa5c30a535f9c1402338
Joao Victor <victor.nascimento@dcomp.ufs.br>
2026-09-03 20:24:57 -0300

    retirada da função consultar_cliente

--- cliente.py
+++ cliente.py
@@ -1,11 +1,6 @@
 def cadastrar_cliente(nome,cpf):
   return nome,cpf
 
-def consultar_cliente(cpf):
-  if cpf == cpf_cadastrado:
-    print ('O cliente tem conta') 
-  else:
-    print ('Cliente não encontrado') 
 
 def criar_conta(cpf,numero_conta,tipo_conta,saldo_inicial):
   return cpf,numero_conta,tipo_conta,saldo_inicial
