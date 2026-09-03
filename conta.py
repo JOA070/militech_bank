239686b1b09532f9dd3f01005621c861f52d19f7
Jesse <jwssik@academico.ufs.br>
2026-09-03 19:58:37 -0300

    Implementa funções da conta

--- conta.py
+++ conta.py
@@ -1,16 +1,10 @@
-def depositar(saldo, deposito):    
-                if deposito <= 0:
-                    print("Inválido")
-                else:
-                    saldo = saldo + deposito
-                return(saldo)
+def depositar(saldo, deposito):
+    saldo = saldo + deposito
+    return saldo
 
 def sacar(saldo, saque):
-                if 0 < saque <= saldo:
-                    saldo = saldo - saque
-                else:
-                    print("Saldo insuficiente")
-                return(saldo)
+    saldo = saldo - saque
+    return saldo
 
 def consultarSaldo(saldo):
-                print("Saldo:",saldo)
+    return saldo
