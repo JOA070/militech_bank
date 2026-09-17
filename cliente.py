32c2e98d75b0e07fddec639a53b876a56391cd93
Joao Victor <victor.nascimento@dcomp.ufs.br>
2026-09-17 10:17:05 -0300

    Aperfeiçoamento do código

--- cliente.py
+++ cliente.py
@@ -1,6 +1,15 @@
+import random
+clientes = []
+contas = []
+
 def cadastrar_cliente(nome,cpf):
-  return nome,cpf
+  cliente = [nome,cpf]
+  clientes.append(cliente)
+  return cliente
 
 
 def criar_conta(cpf,numero_conta,tipo_conta,saldo_inicial):
-  return cpf,numero_conta,tipo_conta,saldo_inicial
+  numero_conta = random.randint(100000,999999)
+  conta = [cpf,numero_conta,tipo_conta,saldo_inicial] 
+  consta.append(conta)
+  return conta
