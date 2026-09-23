3842e9f6039098f099eabd519e579339654c0392
Jesse <jwssik@academico.ufs.br>
2026-09-23 20:18:33 -0300

    Adiciona validações nas operações, transferência e consulta de saldo

--- conta.py
+++ conta.py
@@ -1,10 +1,29 @@
 def depositar(saldo, deposito):
+    if deposito <= 0:
+        print("Valor inválido para depósito!")
+        return saldo
+    
     saldo = saldo + deposito
     return saldo
 
 def sacar(saldo, saque):
+    if saque > saldo or saque <= 0:
+        print("Valor inválido para retirada!")
+        return saldo
+    
     saldo = saldo - saque
     return saldo
 
 def consultarSaldo(saldo):
-    return saldo
+    print("Saldo da conta: R$", saldo)
+
+def transferir(saldo_origem, saldo_destino, valor):
+    if valor > saldo_origem or valor <= 0:
+        print("Valor inválido para transferência!")
+        return saldo_origem, saldo_destino
+
+    saldo_origem = saldo_origem - valor
+    saldo_destino = saldo_destino + valor
+    print("Transferência realizada com sucesso!")
+
+    return saldo_origem, saldo_destino
