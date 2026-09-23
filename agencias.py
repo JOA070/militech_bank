8e8fed8d3e0f9f70122a6236606c4b924dcd72b2
Jesse <jwssik@academico.ufs.br>
2026-09-23 20:22:21 -0300

    Adiciona função para cadastro de agências

---
+++ agencias.py
@@ -0,0 +1,7 @@
+def agencias():
+    lista_agencias = []
+    agencia1 = (1, "Militech Central")
+    agencia2 = (2, "Militech Corpo Plaza")
+    lista_agencias.append(agencia1)
+    lista_agencias.append(agencia2)
+    return lista_agencias
