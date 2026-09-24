54e03edff3eb497a701ef17d821018a494cbe402
Joao Victor <victor.nascimento@dcomp.ufs.br>
2026-09-24 20:02:07 -0300

    ajustes no código para realizar a ordenação das listas

--- cliente.py
+++ cliente.py
@@ -4,46 +4,67 @@ import json
 clientes = []
 contas = []
 
-def cadastrar_cliente(nome,cpf):
-  cliente = (nome,cpf)
-  clientes.append(cliente)
-  return cliente
+def cadastrar_cliente(nome, cpf):
+    cliente = (nome, cpf)
+    clientes.append(cliente)
 
+    for i in range(len(clientes)):
+        for j in range(i + 1, len(clientes)):
+            if clientes[i][0] > clientes[j][0]:
+                clientes[i], clientes[j] = clientes[j], clientes[i]
 
-def criar_conta(cpf,tipo_conta,saldo):
-  numero_conta = random.randint(100000,999999)
-  conta = (cpf,numero_conta,tipo_conta,saldo)
-  contas.append(conta)
-  return conta
+    return cliente
 
-def salvar_clientes(clientes):
-  with open("clientes.json" , "w") as arquivo:
-    json.dump(clientes, arquivo)
+def criar_conta(cpf, tipo_conta, saldo_inicial, agencia):
+    numero_conta = random.randint(100000, 999999)
+    conta = (cpf, numero_conta, tipo_conta, saldo_inicial, agencia)
+    contas.append(conta)
 
+    for i in range(len(contas)):
+        for j in range(i + 1, len(contas)):
+            if contas[i][0] > contas[j][0]:
+                contas[i], contas[j] = contas[j], contas[i]
+
+    return conta
+
+def salvar_clientes():
+    with open("clientes.json", "w") as arquivo:
+        json.dump(clientes, arquivo)
 
 def carregar_clientes():
-  try:
-    with open("clientes.json" , "r") as arquivo:
-      dados = json.load(arquivo)
+    try:
+        with open("clientes.json", "r") as arquivo:
+            dados = json.load(arquivo)
+            clientes.clear()
 
-      clientes.clear()
-      for cliente in dados:
-        clientes.append(tuple(cliente))
-  except FileNotFoundError:
-    pass
+            for cliente in dados:
+                clientes.append(tuple(cliente))
 
-def salvar_contas(contas):
-  with open("contas.json" , "w") as arquivo:
-    json.dump(contas, arquivo)
+            for i in range(len(clientes)):
+                for j in range(i + 1, len(clientes)):
+                    if clientes[i][0] > clientes[j][0]:
+                        clientes[i], clientes[j] = clientes[j], clientes[i]
+
+    except FileNotFoundError:
+        pass
+
+def salvar_contas():
+    with open("contas.json", "w") as arquivo:
+        json.dump(contas, arquivo)
 
 def carregar_contas():
-  try:
-    with open("contas.json" , "r") as arquivo:
-      dados = json.load(arquivo)
+    try:
+        with open("contas.json", "r") as arquivo:
+            dados = json.load(arquivo)
+            contas.clear()
 
-      contas.clear()
+            for conta in dados:
+                contas.append(tuple(conta))
 
-      for conta in dados:
-        contas.append(tuple(conta))
-  except FileNotFoundError:
-    pass
+            for i in range(len(contas)):
+                for j in range(i + 1, len(contas)):
+                    if contas[i][0] > contas[j][0]:
+                        contas[i], contas[j] = contas[j], contas[i]
+
+    except FileNotFoundError:
+        pass
