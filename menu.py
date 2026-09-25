c27580ede110c585c3826f49ba6ca73887522582
PedroIvo3 <ivo.brandao@dcomp.ufs.br>
2026-09-24 22:32:41 -0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,21 @@
+def menu():
+    print("\n" + "=" * 30)
+    print("      OPÇÕES")
+    print("=" * 30)
+    print(" 1 - Cadastrar clientes")
+    print(" 2 - Listar Agências")
+    print(" 3 - Listar Clientes")
+    print(" 4 - Listar Contas")
+    print(" 5 - Depositar")
+    print(" 6 - Sacar")
+    print(" 7 - Transferir")
+    print(" 8 - Consultar Saldo")
+    print(" 9 - Relatório")
+    print(" 0 - Sair")
+    print("=" * 30)
+
+    return (input('O que gostaria de fazer?:'))
+
+        
+
+    
